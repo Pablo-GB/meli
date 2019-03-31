f945c4d85f7ad92cfdb50b41e5778bc865fe2acd
Pablo Bertorello <pgbertorello@gmail.com>
2019-03-31 01:02:09 -0300

    Primer commit

---
+++ meliAutomation/src/test/java/org/meliAutomation/Test1.java
@@ -0,0 +1,61 @@
+package org.meliAutomation;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class Test1 {
+	public String path=System.getProperty("user.dir");
+	public WebDriver driver;
+	public String baseURL="http://mercadolibre.com";
+@BeforeMethod
+public void setBaseURL() {
+	System.setProperty("webdriver.chrome.driver",path+"\\chromeDriver\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get(baseURL);
+	}
+	
+@Test(dataProvider="DP1")  
+public void test(String usrname,String expMsg) 
+{
+	WebElement btnAR= driver.findElement(By.id("AR"));
+	btnAR.click();
+	WebElement btnIngresa=driver.findElement(By.xpath("//*[@id=\"nav-header-menu\"]/a[2]"));
+	btnIngresa.click();
+    //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	//String expectedTitle="¡Hola! Ingresá tu e-mail o usuario";
+	//String currentTitle=driver.getTitle();
+	//Assert.assertTrue(expectedTitle.equals(currentTitle));
+	WebElement userTbox=driver.findElement(By.id("user_id"));
+	WebElement btnContinuar= driver.findElement(By.xpath("//*[@id=\"login_user_form\"]/div[2]/input"));
+	userTbox.sendKeys(usrname);
+	btnContinuar.click();
+	driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+	WebElement msgError=driver.findElement(By.xpath("//*[@id=\"login_user_form\"]/div[1]/div/div/label/span/span/div/div"));
+	String expectedTitle=expMsg;
+	String currentTitle=msgError.getText();
+	Assert.assertTrue(expectedTitle.contains(currentTitle));
+	
+	
+}
+@DataProvider(name="DP1")
+public Object[][] passData()
+{ 
+	Object[][] data= new Object[2][2];
+	data[0][0]="p@gmail.com";
+	data[0][1]="Ingresaste un e-mail o usuario de Mercado Libre Costa Rica";
+	data[1][0]="a@gmail.com";
+	data[1][1]="Ingresaste un e-mail o usuario de Mercado Libre México";
+	return data;
+	}
+
+
+@AfterMethod
+public void quit()
+{
+	driver.quit();
+}
+
+}
---
+++ meliAutomation/src/test/java/org/meliReadExcelLib/ReadExcelConfig.java
@@ -0,0 +1,48 @@
+package org.meliReadExcelLib;
+
+import java.io.File;
+import java.io.FileInputStream;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadExcelConfig {
+	XSSFWorkbook wb;
+	XSSFSheet sheet1;
+	
+	public ReadExcelConfig(String path) 
+	{
+		File src =new File(path);
+		FileInputStream fis;
+		try 
+		{
+			fis = new FileInputStream(src);
+			 wb = new XSSFWorkbook(fis);
+			 
+		} 
+		
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());		
+
+		}
+
+	}
+	
+	
+	public String getData(int sheetNumber, int row, int column)
+	{
+		sheet1= wb.getSheetAt(sheetNumber);
+		String data= sheet1.getRow(row).getCell(column).getStringCellValue();
+		return data;
+	
+	}
+	
+	public int getSheetRowNumber(int sheetNumber)
+	{
+		int rownumber=wb.getSheetAt(sheetNumber).getLastRowNum();
+		rownumber=rownumber+1;
+		return rownumber;
+		
+	}
+
+}
