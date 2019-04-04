391d1cde5abecf21ad255c6d35e6a9fa17543c8e
Pablo Bertorello <pgbertorello@gmail.com>
2019-04-04 02:59:16 -0300

    Primera version de clase base funcionando en test3.java

--- meliAutomation/src/test/java/org/meliAutomation/Test3.java
+++ meliAutomation/src/test/java/org/meliAutomation/Test3.java
@@ -2,6 +2,7 @@ package org.meliAutomation;
 
 import java.util.concurrent.TimeUnit;
 
+import org.meli.base.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,30 +11,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Test3 {
 	
 	public String path=System.getProperty("user.dir");
-	public WebDriver driver;
+	TestBase base = new TestBase();
 	public String baseURL="http://google.com";
 	
 @BeforeMethod
 public void setBaseURL() {
-	System.setProperty("webdriver.chrome.driver",path+"\\chromeDriver\\chromedriver.exe");
-	driver = new ChromeDriver();
-	driver.get(baseURL);
+//	System.setProperty("webdriver.chrome.driver",path+"\\chromeDriver\\chromedriver.exe");
+//	base.openDriver(baseURL);
+//	base.driver.get(base.url);
+	System.out.print("before method");
 	}
 	
 @Test 
 public void test() 
 {//algunos comentarios por aqui hacen a un nuevo comit
-	WebElement textboxSearch= driver.findElement(By.name("q"));
+	WebElement textboxSearch= base.driver.findElement(By.name("q"));
 	textboxSearch.sendKeys("talleres");
 	textboxSearch.sendKeys(Keys.ENTER);
-	driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-	String currentTitle= driver.getTitle();
+	base.driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+	String currentTitle= base.driver.getTitle();
 	String expectedTitle="talleres - Buscar con Google";
 	Assert.assertTrue(currentTitle.equals(expectedTitle));
 	
@@ -44,7 +45,7 @@ public void test()
 @AfterMethod
 public void quit()
 {
-	driver.quit();
+	base.driver.quit();
 }
 
 }
